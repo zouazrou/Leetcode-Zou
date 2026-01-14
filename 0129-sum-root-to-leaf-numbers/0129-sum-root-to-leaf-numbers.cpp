7a027960d8408f83412c45b419b0050807ad8137
Zakaria Ouazrou <zakariawazrou@gmail.com>
2026-01-14 11:46:18 +0100

    Time: 0 ms (100%), Space: 12.2 MB (15.73%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,16 +1,36 @@
 class Solution {
 private:
-    int dfs(TreeNode* root, int num) {
-        if (!root)
-            return 0;
-        if (!root->left && !root->right)
-            return num * 10 + root->val;
-        return dfs(root->left, num * 10 + root->val)
-              +dfs(root->right, num * 10 + root->val);
+    // int dfs(TreeNode* root, int num) {
+    //     if (!root)
+    //         return 0;
+    //     if (!root->left && !root->right)
+    //         return num * 10 + root->val;
+    //     return dfs(root->left, num * 10 + root->val)
+    //           +dfs(root->right, num * 10 + root->val);
+    // }
+    int bfs(TreeNode* root) {
+        int ans = 0;
+        queue<pair<TreeNode*, int>> q;
+        pair<TreeNode*, int> curr;
+        q.push({root, root->val});
+        while (!q.empty()) {
+            curr = q.front();
+            q.pop();
+            if (!curr.first->left && !curr.first->right)
+                ans += curr.second;
+            if (curr.first->left)
+                q.push({curr.first->left,
+                        curr.first->left->val + 10 * curr.second});
+            if (curr.first->right)
+                q.push({curr.first->right,
+                        curr.first->right->val + 10 * curr.second});
+        }
+        return ans;
     }
 
 public:
     int sumNumbers(TreeNode* root) {
-        return dfs(root, 0);
+        // return dfs(root, 0);
+        return bfs(root);
     }
 };

e44da532d774b21627973236021673946a63d209
Zakaria Ouazrou <zakariawazrou@gmail.com>
2026-01-17 11:14:58 +0100

    Time: 4 ms (1.42%), Space: 15.8 MB (31.91%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (nums.size() == 1)
+            return ;
+        auto i = nums.end();
+        i--;
+        while (1)
+        {
+            auto j = i;
+            i--;
+            if (*i < *j)
+            {
+                auto k = nums.end();
+                k--;
+                while (*i >= *k)
+                    k--;
+                cout << "ye " << *i << ' ' << *k;
+                swap(*i, *k);
+                reverse(j, nums.end());
+                return ;
+            }
+            if (i == nums.begin())
+            {
+                reverse(i, nums.end());
+                return ;
+            }
+        }
+    }
+};

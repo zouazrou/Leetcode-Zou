c2d01b5ccbbf5ad328d764f2ba5f0b75db0fc2bb
Zakaria Ouazrou <zakariawazrou@gmail.com>
2026-01-18 11:27:18 +0100

    Time: 59 ms (14.39%), Space: 7.9 MB (95.07%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,19 +1,55 @@
 class Solution {
-public:
-    string solve(string ans, int n, int& k) {
-        if (ans.size() == n) {
-            k--;
-            return (k ? "" : ans);
+private:
+    void ft_next_permutation(vector<int>& nums) {
+        if (nums.size() == 1)
+            return;
+        auto i = nums.end();
+        i--;
+        while (1) {
+            auto j = i;
+            i--;
+            if (*i < *j) {
+                auto k = nums.end();
+                k--;
+                while (*i >= *k)
+                    k--;
+                swap(*i, *k);
+                reverse(j, nums.end());
+                return;
+            }
+            if (i == nums.begin()) {
+                reverse(i, nums.end());
+                return;
+            }
         }
-        string res;
-        for (int num = 1; num <= n; num++) {
-            if (ans.find(char(num + '0')) != string::npos)
-                continue;
-            res = solve(ans + char(num + '0'), n, k);
-            if (!res.empty())
-                return res;
-        }
-        return "";
     }
-    string getPermutation(int n, int k) { return solve("", n, k); }
+    // **USING DFS**
+    // string solve(string ans, int n, int& k) {
+    //     if (ans.size() == n) {
+    //         k--;
+    //         return (k ? "" : ans);
+    //     }
+    //     string res;
+    //     for (int num = 1; num <= n; num++) {
+    //         if (ans.find(char(num + '0')) != string::npos)
+    //             continue;
+    //         res = solve(ans + char(num + '0'), n, k);
+    //         if (!res.empty())
+    //             return res;
+    //     }
+    //     return "";
+    // }
+public:
+    string getPermutation(int n, int k) {
+        vector<int> nums(n);
+        iota(nums.begin(), nums.end(), 1);
+        while (--k) {
+            ft_next_permutation(nums);
+        }
+        string ans;
+        for (int i = 0; i < n; i++)
+            ans += char(nums[i] + '0');
+        return ans;
+        // return solve("", n, k);
+    }
 };
